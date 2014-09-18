897bbbd455794106e8dbec1e49fbf6a4736894d0
trevogrif7 <trevorgriffin@Trevors-MacBook-Pro.local>
2014-09-17 23:03:39 -0500

    added a Git Test class

---
+++ GIT Test/TEGTestGITFile.h
@@ -0,0 +1,13 @@
+//
+//  TEGTestGITFile.h
+//  GIT Test
+//
+//  Created by Trevor Griffin on 9/17/14.
+//  Copyright (c) 2014 TREVOR E GRIFFIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TEGTestGITFile : NSObject
+
+@end
---
+++ GIT Test/TEGTestGITFile.m
@@ -0,0 +1,13 @@
+//
+//  TEGTestGITFile.m
+//  GIT Test
+//
+//  Created by Trevor Griffin on 9/17/14.
+//  Copyright (c) 2014 TREVOR E GRIFFIN. All rights reserved.
+//
+
+#import "TEGTestGITFile.h"
+
+@implementation TEGTestGITFile
+
+@end
